95ae34d719d7d6ae8f86146760fe09123bdefbc1
Gaurav Kumar <vermagaurav851@gmail.com>
2024-06-12 19:16:46 +0530

    update

--- src/section/projects/projects.jsx
+++ src/section/projects/projects.jsx
@@ -13,14 +13,14 @@ function Projects() {
           src={Tictactoe}
           lnk={"https://vermagaur851.github.io/tic-tac-toe-game/"}
           heading={"Unbeatable TicTacToe"}
-          description={`Built using HTML, CSS and JavaScript, for unbeatable purpose I have used minimax Algo with α-β Pruning`}
+          description={`Built using HTML, CSS and JavaScript, for unbeatable purpose I have used minimax Algo`}
           git={"https://github.com/vermagaur851/tic-tac-toe-game"}
         />
         <ProjectCard
           src={Music_Institute}
           lnk={"https://music-app-rose-zeta.vercel.app/"}
           heading={"Website for Music Institute"}
-          description={"This website is created using Nextjs and Aceternity UI.This website is mostly a showcase of UI/UX skill in Nextjs"}
+          description={"Created using Nextjs and Aceternity UI.This website is just a showcase of my Nextjs skill"}
           git={"https://github.com/vermagaur851/music-app"}
         />
       </div>
